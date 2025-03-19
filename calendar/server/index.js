f9fe36d174573b7bb4848b56a3951c6071feab59
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-19 19:53:16 +0200

    Added some prettier cofig

--- calendar/server/index.js
+++ calendar/server/index.js
@@ -1,18 +1,21 @@
 const port = 7777;
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
 
-const bodyParser = require("body-parser");
-const path = require("path");
+const bodyParser = require('body-parser');
+const path = require('path');
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log(__dirname + "../client");
+console.log(__dirname + '../client');
+console.log('"fs"ff');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
   res.render('index', {});
-})
+});
 
-app.listen(port, () => { console.log(`Now listening on http://localhost:${port}`); });
+app.listen(port, () => {
+  console.log(`Now listening on http://localhost:${port}`);
+});
